dce7d5fce2558260e2351ffc5c7bb1842aba04c3
Ailton josé santos silva júnior <151879636+ailtonXL@users.noreply.github.com>
2024-11-21 19:26:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toogleMode(){
+    const html=document.documentElement
+
+    if(html.classList.contains('dark')){
+        html.classList.remove('dark')
+    }else{
+        html.classList.add('dark')
+    }
+
+}
